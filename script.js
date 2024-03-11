e016865aba78647be4916c16e14fe50fc139de9f
CRXAhsan <ahsantariq669@gmail.com>
2024-03-11 10:52:33 +0500

    fomratting Commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 window.addEventListener("load", checkInternetConnection);
 
-function checkInternetConnection(){
+function checkInternetConnection() {
 
     const statusText = document.getElementById('statusText');
     const ipAddressText = document.getElementById('ipAddressText');
@@ -10,31 +10,29 @@ function checkInternetConnection(){
 
     statusText.textContent = 'Checking.....';
 
-    if(navigator.onLine)
-    {
+    if (navigator.onLine) {
         fetch('https://api.ipify.org?format=json')
-        .then((response)=> response.json())
-        .then((data)=>{
+            .then((response) => response.json())
+            .then((data) => {
 
-            ipAddressText.textContent = data.ip;
-            statusText.textContent = 'Connected';
-
-
-            const connection = navigator.connection;
-
-            const networkStrength = connection ?connection.downlink +'Mbps' : 'Unknown';
+                ipAddressText.textContent = data.ip;
+                statusText.textContent = 'Connected';
 
 
-            networkStrengthText.textContent = networkStrength;
-        })
-        .catch(()=>{
-            statusText.textContent = 'Disconnected'
-            ipAddressText.textContent = '-';
-            networkStrengthText.textContent = '-';
-        })
+                const connection = navigator.connection;
+
+                const networkStrength = connection ? connection.downlink + 'Mbps' : 'Unknown';
+
+
+                networkStrengthText.textContent = networkStrength;
+            })
+            .catch(() => {
+                statusText.textContent = 'Disconnected'
+                ipAddressText.textContent = '-';
+                networkStrengthText.textContent = '-';
+            })
     }
-    else
-    {
+    else {
         statusText.textContent = 'Disconnected'
         ipAddressText.textContent = '-';
         networkStrengthText.textContent = '-';
